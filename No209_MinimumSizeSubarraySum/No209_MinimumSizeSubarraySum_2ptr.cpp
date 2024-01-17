fde33847cfe18d0e2c5598351cba50d9dcea1b39
施嘉诚 <jiachengshirick@gmail.com>
2024-01-17 15:04:32 -0800

    Refresh my answer of Question 209

--- No209_MinimumSizeSubarraySum/No209_MinimumSizeSubarraySum_2ptr.cpp
+++ No209_MinimumSizeSubarraySum/No209_MinimumSizeSubarraySum_2ptr.cpp
@@ -1,15 +1,17 @@
-int minSubArrayLen(int s, vector<int>& nums)
-{
-    int n = nums.size();
-    int ans = INT_MAX;
-    int left = 0;
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        sum += nums[i];
-        while (sum >= s) {
-            ans = min(ans, i + 1 - left);
-            sum -= nums[left++];
+class Solution {
+public:
+    int minSubArrayLen(int target, vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        int left = 0;
+        int Output = INT_MAX;
+        for (int i = 0; i < n; i++ ){
+            sum += nums[i];
+            while(sum >= target){
+                Output = min(Output, i + 1 - left);
+                sum-=nums[left++];
+            }
         }
+        return Output == INT_MAX ? 0 : Output;
     }
-    return (ans != INT_MAX) ? ans : 0;
-}
+};
